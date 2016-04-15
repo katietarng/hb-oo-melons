3948c095d0088c074676610110178fb451c65c6d
Hackbright Student <info@hackbrightacademy.com>
2016-04-14 17:15:02 -0700

    Refactored melon order classes

---
+++ melons.py
@@ -0,0 +1,43 @@
+"""This file should have our order classes in it."""
+
+class AbstractMelonOrder(object):
+    """ Parent class for melon orders. """
+
+    def __init__(self, species, qty, country_code = None): #Country_code may need to be optional param
+
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.country_code = country_code
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
